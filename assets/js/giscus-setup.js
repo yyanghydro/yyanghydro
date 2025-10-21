f3c445429350138bf4b851180d3ef37100f1a875
yyanghydro <yyang90hku@gmail.com>
2025-10-21 20:06:14 +0000

    Deploying to gh-pages from @ yyanghydro/yyanghydro.github.io@eb71e0a9b4203d4c50ed46b2546c20207fe2a56f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yyanghydro/yyanghydro",
+    "data-repo": "yyanghydro/yyanghydro.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
